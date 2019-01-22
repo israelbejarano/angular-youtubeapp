d1313155f708764d8ed44f9d1c846ddba1182efc
israel <israel.bejarano.a@gmail.com>
2019-01-22 21:51:54 +0100

    create services and getVideos

---
+++ src/app/services/youtube.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class YoutubeService {
+
+  private youtubeUrl = 'https://www.googleapis.com/youtube/v3';
+  private apikey = 'your aki key';
+  private playlist = 'playlist id';
+  private nextPageToken = '';
+
+  constructor(private http: HttpClient) { }
+
+  getVideos() {
+
+    const query = '/playlistItems';
+    const params = new HttpParams()
+    .set('part', 'snippet')
+    .set('maxResults', '10')
+    .set('playlistId', this.playlist)
+    .set('key', this.apikey);
+
+    return this.getQuery(query, params).pipe(map(res => {
+      console.log(res);
+      this.nextPageToken = res['nexPageToken'];
+      let videos: any[] = [];
+      for (let video of res['items']) {
+        let snippet = video['snippet'];
+        videos.push(snippet);
+      }
+      return videos;
+    }));
+  }
+
+  getQuery(query: string, params: HttpParams) {
+     const url = `${this.youtubeUrl}${query}`;
+     return this.http.get(url, { params });
+  }
+}
